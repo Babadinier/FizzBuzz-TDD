97d9277f33b6de0e5d8e239ce83e1bc94e2a6f1d
Guillaume BADINIER <gub@softfluent.com>
2019-04-24 13:55:59 +0200

    First commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FizzBuzzServices;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static readonly FizzBuzzer fizzBuzzer = new FizzBuzzer();
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                Console.WriteLine(fizzBuzzer.GetValue(i));
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FizzBuzzServices/FizzBuzzer.cs
@@ -0,0 +1,20 @@
+﻿namespace FizzBuzzServices
+{
+    public class FizzBuzzer
+    {
+        public string GetValue(int number)
+        {
+            string result = string.Empty;
+            if (number % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                result += "Buzz";
+            }
+            
+            return result == string.Empty ? number.ToString() : result;
+        }
+    }
+}
